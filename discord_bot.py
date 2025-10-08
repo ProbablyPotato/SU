aafb979de7e97975994042e998f7abddbcb6828f
ProbablyPotato <138603830+ProbablyPotato@users.noreply.github.com>
2025-10-08 12:10:59 +0100

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -36,7 +36,62 @@ async def on_ready():
     print(f"Is now running!")
     #create scheduler
     scheduler = AsyncIOScheduler()
-    await start_schedule(scheduler)
+    await start_schedule()
+
+
+
+# Function to apply the role to all members with the specified role
+async def apply_role():
+    guild = client.get_guild(guild_id)
+    apply_role = guild.get_role(CLOSED_ROLE_ID)
+    usual_role = guild.get_role(STANDARD_ROLE_ID)
+
+    for member in guild.members:
+        if usual_role in member.roles and apply_role not in member.roles:
+            await member.add_role(apply_role)
+
+    print(f"Applied role {apply_role.name} to all members with role {usual_role.name}")
+
+# Function to remove the role from all members
+async def remove_role():
+    guild = client.get_guild(guild_id)
+    apply_role = guild.get_role(CLOSED_ROLE_ID)
+    usual_role = guild.get_role(STANDARD_ROLE_ID)
+
+    for member in guild.members:
+        if usual_role in member.roles and apply_role in member.roles:
+            await member.remove_role(apply_role)
+
+    print(f"Removed role {apply_role.name} from all members")
+
+
+# Function to start the server lockdown between given hours
+async def start_schedule():
+    print(f'Bot scheduler has been triggered!')
+   
+    # Set up the scheduler to run the functions at the specified times
+    scheduler.add_job(apply_role, 'cron', hour=APPLY_TIME.split(':')[0], minute=APPLY_TIME.split(':')[1])
+    scheduler.add_job(remove_role, 'cron', hour=REMOVE_TIME.split(':')[0], minute=REMOVE_TIME.split(':')[1])
+    scheduler.start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 client.run(TOKEN)
