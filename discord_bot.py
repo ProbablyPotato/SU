60e2876aa92119a58d262c1c663c686e7a7a14ad
ProbablyPotato <138603830+ProbablyPotato@users.noreply.github.com>
2025-10-08 13:16:51 +0100

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -31,7 +31,7 @@ STANDARD_ROLE_NAME = 'members'
 
 
 # Set the times when server should shut down and reopen
-APPLY_TIME = '11:58'  # 10 PM
+APPLY_TIME = '12:18'  # 10 PM
 REMOVE_TIME = '17:33'  # 7 AM
 
 #create scheduler
